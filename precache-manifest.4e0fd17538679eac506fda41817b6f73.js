c24a9a24c8d42c1c6b1f6bddefbb92d873d5f44d
Ritesh Jaiswal <riteshjaizz@gmail.com>
2022-08-09 19:46:19 +0530

    Updates

---
+++ precache-manifest.4e0fd17538679eac506fda41817b6f73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d86bfc25be96800f30fa2cdf51024b9",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/Keeper-App/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "f950c6ffa3418a3f280e",
+    "url": "/Keeper-App/static/js/main.f164221e.chunk.js"
+  },
+  {
+    "revision": "4997685bab9cff726bba",
+    "url": "/Keeper-App/static/js/runtime-main.31b25eb3.js"
+  }
+]);
